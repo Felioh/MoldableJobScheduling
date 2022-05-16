4c5001623ebbf44a53fdf619009d4b77ea334fb4
suwbl529 <f.ohnesorge@bwl.uni-kiel.de>
2022-05-16 22:36:18 +0200

    added Knapsack

--- src/Algo.java
+++ src/Algo.java
@@ -18,11 +18,6 @@ public class Algo {
 
         System.out.println(printSchedule.printTwoShelves(bigJobs, (int) d));
 
-        //find all jobs that cant be scheduled in d/2 time.
-        // int cap = Arrays.stream(bigJobs).filter(j -> j.getAllotedMachines() == -1).mapToInt(j -> I.canonicalNumberMachines(j.getId(), d)).reduce(0, Integer::sum); //reduce(0, (a, b) -> I.canonicalNumberMachines(a.getId(), d) + I.canonicalNumberMachines(b.getId(), d));
-        //..and then remove them from the knapsack problem.
-        // bigJobs = Arrays.stream(bigJobs).filter(j -> j.getAllotedMachines() != -1).toArray(Job[] :: new);
-
         //transform to knapsack problem
         int[] profit = new int[bigJobs.length];
         int[] weight = new int[bigJobs.length];
@@ -45,56 +40,13 @@ public class Algo {
 
         }
 
-        bigJobs = dynamicKnapsack(bigJobs, weight, profit, bigJobs.length, I.getM(), I, d);
-
+        bigJobs = MyMath.dynamicKnapsack(bigJobs, weight, profit, bigJobs.length, I.getM(), I, d);
+        Job[] convRes = MyMath.knapsackConvolution(bigJobs, weight, profit, bigJobs.length, I.getM(), I, d);
 
         System.out.println();
         System.out.println(printSchedule.printTwoShelves(bigJobs, (int) d));
+        System.out.println(printSchedule.printTwoShelves(convRes, (int) d));
 
     }
 
-    private static Job[] dynamicKnapsack(Job[] jobs, int[] wt, int[] val, int n, int W, Instance I, double d) {
-        int i, w;
-        int K[][] = new int[n + 1][W + 1];
- 
-        // Build table K[][] in bottom up manner
-        for (i = 0; i <= n; i++) {
-            for (w = 0; w <= W; w++) {
-                if (i == 0 || w == 0)
-                    K[i][w] = 0;
-                else if (wt[i - 1] <= w)
-                    K[i][w] = Math.max(val[i - 1] +
-                              K[i - 1][w - wt[i - 1]], K[i - 1][w]);
-                else
-                    K[i][w] = K[i - 1][w];
-            }
-        }
- 
-        // stores the result of Knapsack
-        int res = K[n][W];
- 
-        w = W;
-        for (i = n; i > 0 && res > 0; i--) {
- 
-            // either the result comes from the top
-            // (K[i-1][w]) or from (val[i-1] + K[i-1]
-            // [w-wt[i-1]]) as in Knapsack table. If
-            // it comes from the latter one/ it means
-            // the item is included.
-            if (res == K[i - 1][w])
-                continue;
-            else {
- 
-                // This item is included.
-                jobs[i - 1].setAllotedMachines(I.canonicalNumberMachines(jobs[i - 1].getId(), d));    //allot job to machines respecting d as a threshold
- 
-                // Since this weight is included its
-                // value is deducted
-                res = res - val[i - 1];
-                w = w - wt[i - 1];
-            }
-        }
-        return jobs;
-    }
-    
 }
---
+++ src/util/ConvolutionElement.java
@@ -0,0 +1,21 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ConvolutionElement {
+
+    private int profit = 0;
+    private List<Job> jobs;
+
+    public ConvolutionElement(int profit, List<Job> jobs) {
+        this.profit = profit;
+        this.jobs = jobs;
+    }
+    
+}
